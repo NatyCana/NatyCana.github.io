e63f4fad8da262eb8a957ba5f191bbc41f364cef
NatyCana <naty@MBPdiNaCanonica.fritz.box>
2016-09-14 17:53:47 +0200

    Update all new website

---
+++ css/codice.css
@@ -0,0 +1,551 @@
+/*-------------------Media-----------------------*/
+/* Extra small devices (phones, less than 768px) */
+@media (max-width: 768px) { 
+    html {
+    height: 100%;
+    width: 100%;
+    margin:0;
+    padding:0;
+    position: relative;
+    }
+    body {
+    height: 100%;
+    width: 100%;
+    margin:0;
+    padding:0;
+    position: relative;
+    overflow-y: scroll;
+    }
+    #colonna{
+        margin: 0;
+        padding: 0;
+    }
+    h1{
+        font-size: 40px;
+    }
+    h4{
+        font-size: 14px;
+        font-family: 'EurostileLTStd-Demi';
+    }
+  /*===== HOME =====*/
+    #home{
+        padding-top: 15%;
+    }
+    #img-logo{
+        width: 100%;
+        height: 100%;
+    }
+
+    #diploma-title{
+        font-size: 16px;
+        margin: 0;
+        padding-top: 2%;
+
+    }
+    #logo-name{
+        padding-top: 25%;
+        padding-bottom: 25%;
+    }
+    /*===== ABOUT ME =====*/
+
+    #bg-aboutMe{
+        margin-top: 10px;
+    }
+    #header-aboutMe{
+        padding-top: 15%;
+    }
+    #container-aboutMe{
+        padding-top: 2%;
+        padding-right: 5%;
+    }
+    #photo_aboutMe {
+        padding-top: 0%;
+        width: 100%;
+        height: 100%;
+        margin-left: 80px;
+    }
+    #title-description h4{
+        font-size: 12px;
+        padding-left: 5%;
+        padding-right: 5%;
+        padding-bottom: 0;
+    }
+    #text-description p {
+        padding-left: 5%;
+        padding-right: 5%;
+        font-size: 11px;
+    }
+    #btn-goToSkills {
+        margin-left: 10px;
+        margin-top: -5px;
+        width: 120px;
+        padding: 12px 25px;
+        font-weight: 500;
+        font-size: 11px;
+    }
+    /*===== EDUCATION =====*/
+    #education{
+        margin-top: 50px;
+    }
+    #header-education{
+        padding-top: 2%;
+    }
+    #container-education{
+        text-align: center;
+        padding-top: 0%;
+        padding-bottom: 0%;
+    }
+    #rombo{
+        height: 25%;
+        width: 25%;
+    }
+    .education-year h4{
+        font-family: 'EurostileLTStd-Demi';
+    }
+    .name-school{
+        padding-top: 0%;
+    }
+    .name-school h4{
+       
+        font-family: 'EurostileLTStd-Demi';
+    }
+    #education-HEIG{
+        padding-bottom: 5%;
+    }
+    .education-text p{
+        font-size: 11px;
+    }
+
+     /*===== SKILLS =====*/
+    #skills{
+        margin-top: -50px;
+    }
+    #description-skills{
+        font-size: 11px;
+    }
+
+    /*===== PROJECT =====*/
+    #project{
+        margin-top: 35px;
+    }
+    #header-project{
+        padding-top: 15%;
+    }
+    .prj{
+        padding-top: 25%;
+    }
+    .prj-row-two{
+        padding-top: 35%;
+    }
+    .img-prj{
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+
+    }
+
+    /*===== LANGUAGES =====*/
+    #languages{
+        margin-top: 0px;
+    }
+    #header-languages{
+        padding-top: 20%;
+    }
+    #container-languages{
+        padding-top: 8%;
+        text-align: center;
+    }
+    .img-flag{
+        width: 50%;
+        height: 50%;
+    }
+
+    /*#english-flag{
+        padding-top: 8%;
+    }
+    #german-flag{
+        padding-top: 8%;
+    }*/
+    .title-lang{
+        padding-top: 0;
+    }
+    .lang-desc{
+        font-size: 10px;
+    }
+
+    /*===== CONTACT =====*/
+    #contact{
+        margin-top: 150px;
+    }
+    #header-contact{
+        padding-top: 20%;
+    }
+    #email-txt{
+        font-size: 14px;
+    }
+    #linkedin-txt{
+        font-size: 14px;
+    }
+
+
+}
+
+/* Small devices (tablets, 768px and up) */
+@media (min-width: 768px){ 
+    #colonna{
+        text-align: left;
+    }
+
+    /*===== HOME =====*/
+    #home{
+        padding-top: 10%;
+    }
+    #img-logo{
+        width: 100%;
+        height: 100%;
+        padding-top: 15%;
+        padding-bottom: 15%;
+    }
+    #navbar-home{
+        padding: 0;
+    }
+    .navbar-nav {
+        margin: 7.5px -15px;
+        margin-top: 7.5px;
+        margin-right: -15px;
+        margin-bottom: 7.5px;
+        margin-left: -15px;
+    }
+
+    /*===== ABOUT ME =====*/
+    #header-aboutMe{
+        padding-top: 25%;
+    }
+    #container-aboutMe{
+        padding-top: 5%;
+        padding-right: 5%;
+    }
+    #photo_aboutMe {
+        width: 65%;
+        height: 65%;
+        margin-left: 50px;
+    }
+    #title-description h4{
+        padding-left: 10%;
+        padding-right: 0%;
+        
+    }
+    #text-description p {
+        padding-left: 10%;
+        padding-right: 0%;
+
+    }
+    #btn-goToSkills {
+        margin-left: 30px;
+    }
+
+     /*===== EDUCATION =====*/
+    #education{
+        margin-top: 200px;
+    }
+    #header-education{
+        padding-top: 5%;
+    }
+    #container-education{
+        padding-top: 5%
+    }
+    #rombo{
+        height: 10%;
+        width: 10%;
+    }
+    .name-school{
+        padding-top: 0%;
+    }
+    #education-HEIG{
+        padding-bottom: 5%;
+    }
+    .education-text{
+        padding-left: 10%;
+        padding-right: 10%;
+    }
+
+
+    /*===== SKILLS =====*/
+    .skills-item{
+        padding-top: 5%;
+        padding-bottom: 5%;
+    }
+    .skills-title-info {
+        padding-bottom: 10%;
+    }
+
+    /*===== PROJECT =====*/
+    #header-project{
+        padding-top: 15%;
+    }
+    .prj{
+        padding-top: 15%;
+    }
+    .prj-row-two{
+        padding-top: 25%;
+    }
+    .img-prj{
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+
+    }
+    /*===== LANGUAGES =====*/
+    #languages{
+        margin-top: 80px;
+    }
+    #header-languages{
+        padding-top: 15%;
+    }
+    #container-languages{
+        padding-top: 15%;
+    }
+    .bw-flag{
+        width: 100%;
+        height: 100%;
+    }
+    .color-flag{
+        width: 100%;
+        height: 100%;
+    }
+    #english-flag{
+        padding-top: 8%;
+    }
+    #german-flag{
+        padding-top: 8%;
+    }
+
+    /*===== CONTACT =====*/
+    #contact{
+        margin-top: 100px;
+    }
+    #header-contact{
+        padding-top: 20%;
+    }
+    #email{
+        padding-top: 30%;
+    }
+
+}
+@media (max-width: 991px) { 
+    #colonna{
+        text-align: left;
+    }
+
+    /*===== HOME =====*/
+    #home{
+        padding-top: 10%;
+    }
+    #img-logo{
+        width: 100%;
+        height: 100%;
+        padding-top: 15%;
+        padding-bottom: 15%;
+    }
+    #navbar-home{
+        padding: 0;
+    }
+    .navbar-nav {
+        margin: 7.5px -15px;
+        margin-top: 7.5px;
+        margin-right: -15px;
+        margin-bottom: 7.5px;
+        margin-left: -15px;
+    }
+
+    /*===== ABOUT ME =====*/
+    #header-aboutMe{
+        padding-top: 25%;
+    }
+    #container-aboutMe{
+        padding-top: 5%;
+        padding-right: 5%;
+    }
+    #photo_aboutMe {
+        width: 65%;
+        height: 65%;
+        margin-left: 50px;
+    }
+    #title-description h4{
+        padding-left: 10%;
+        padding-right: 0%;
+        
+    }
+    #text-description p {
+        padding-left: 10%;
+        padding-right: 0%;
+
+    }
+    #btn-goToSkills {
+        margin-left: 30px;
+    }
+
+     /*===== EDUCATION =====*/
+    #education{
+        margin-top: 200px;
+    }
+    #header-education{
+        padding-top: 5%;
+    }
+    #container-education{
+        padding-top: 5%
+    }
+    #rombo{
+        height: 10%;
+        width: 10%;
+    }
+    .name-school{
+        padding-top: 0%;
+    }
+    #education-HEIG{
+        padding-bottom: 5%;
+    }
+    .education-text{
+        padding-left: 10%;
+        padding-right: 10%;
+    }
+
+
+    /*===== SKILLS =====*/
+    .skills-item{
+        padding-top: 5%;
+        padding-bottom: 5%;
+    }
+    .skills-title-info {
+        padding-bottom: 10%;
+    }
+
+    /*===== PROJECT =====*/
+    #header-project{
+        padding-top: 15%;
+    }
+    .prj{
+        padding-top: 15%;
+    }
+    .prj-row-two{
+        padding-top: 25%;
+    }
+    .img-prj{
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+
+    }
+    /*===== LANGUAGES =====*/
+    #languages{
+        margin-top: 80px;
+    }
+    #header-languages{
+        padding-top: 15%;
+    }
+    #container-languages{
+        padding-top: 15%;
+    }
+    .bw-flag{
+        width: 100%;
+        height: 100%;
+    }
+    .color-flag{
+        width: 100%;
+        height: 100%;
+    }
+    #english-flag{
+        padding-top: 8%;
+    }
+    #german-flag{
+        padding-top: 8%;
+    }
+
+    /*===== CONTACT =====*/
+    #contact{
+        margin-top: 100px;
+    }
+    #header-contact{
+        padding-top: 20%;
+    }
+    #email{
+        padding-top: 30%;
+    }
+
+}
+@media (min-width: 992px){
+    html {
+    height: 100%;
+    width: 100%;
+    margin:0;
+    padding:0;
+    position: relative;
+    }
+    body {
+    height: 100%;
+    width: 100%;
+    margin:0;
+    padding:0;
+    position: relative;
+    overflow-y: scroll;
+    }
+    #colonna{
+        margin: 0;
+        padding: 0;
+    }
+    /*===== HOME =====*/
+    #home{
+        padding-top: 10%;
+    }
+    #img-logo{
+        width: 100%;
+        height: 100%;
+        padding-top: 15%;
+        padding-bottom: 15%;
+    }
+    #navbar-home{
+        padding: 0;
+    }
+    .navbar-nav {
+        margin: 7.5px -15px;
+        margin-top: 7.5px;
+        margin-right: -15px;
+        margin-bottom: 7.5px;
+        margin-left: -15px;
+    }
+    /*===== ABOUT ME =====*/
+    #header-aboutMe{
+        padding-top: 25%;
+    }
+    #container-aboutMe{
+        padding-top: 5%;
+        padding-right: 5%;
+    }
+    #photo_aboutMe {
+        width: 65%;
+        height: 65%;
+        margin-left: 50px;
+    }
+    #title-description h4{
+        padding-left: 10%;
+        padding-right: 0%;
+        
+    }
+    #text-description p {
+        padding-left: 10%;
+        padding-right: 0%;
+
+    }
+    #btn-goToSkills {
+        margin-left: 30px;
+    }
+
+}
+
+
+    
+
+
+
+
+
