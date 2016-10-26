59299eaf8c5f9911af5169606ab4c55ed03007d5
NatyCana <naty@MBPdiNaCanonica.fritz.box>
2016-10-26 14:26:17 +0200

    new update

--- js/main.js
+++ js/main.js
@@ -5,8 +5,14 @@ $(document).ready(function() {
     window_height = $(window).height();
 
     $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
+        $('[data-toggle="tooltip"]').tooltip()
+    });
+
+    /*=========== Section hover ===========*/
+    $("#skills").hover(function(){
+        /**/
+        console.log("skills");
+    })
 });
 
 /*=========== BASIC FUNCTION ===========*/
@@ -30,5 +36,24 @@ function LowScroll(){
         window.location.hash = hash;
         });
     });
+    $("footer a").on('click', function(event) {
+
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 500, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+        });
+    });
 
 }
+ 
