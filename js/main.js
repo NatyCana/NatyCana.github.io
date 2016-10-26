9e70d3e291b61c0b6883c7636593731316d4d5f7
NatyCana <naty@MBPdiNaCanonica.fritz.box>
2016-10-26 11:44:54 +0200

    add website

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var window_height;
+
+$(document).ready(function() {
+    LowScroll();
+    window_height = $(window).height();
+
+    $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+});
+
+/*=========== BASIC FUNCTION ===========*/
+function LowScroll(){
+    
+    $("#home a").on('click', function(event) {
+
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 1000, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+        });
+    });
+
+}
