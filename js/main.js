859c0b36fda94799e71b330b6b1f55f9a8c28e68
NatyCana <natalie.canonica@gmail.com>
2018-11-08 15:24:15 +0100

    new color and font

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
-(function($) {
-  "use strict"; // Start of use strict
+$(document).ready(function(){
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 54)
-        }, 1200, "easeInOutExpo");
-        return false;
-      }
-    }
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+
+      });
+    } // End if
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
@@ -47,4 +54,5 @@
     $(".navbar").removeClass("d-none");
   })
 
-})(jQuery); // End of use strict
+
+});
